e8b3d733a1cbb3f787cf07d6187ba278b5e32197
Bo Shang <cnpcshangbo@gmail.com>
2023-01-31 14:03:53 -0500

    posts

--- src/App.js
+++ src/App.js
@@ -1,7 +1,54 @@
 import './App.css';
 // after other import statements
 // import { BrowserRouter as Router } from 'react-router-dom';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link, Outlet, useParams } from 'react-router-dom';
+
+const BlogPosts = {
+  '1': {
+    title: 'First Blog Post',
+    description: 'Lorem ipsum dolor sit amet, consectetur adip.'
+  },
+  '2': {
+    title: 'Second Blog Post',
+    description: 'Hello React Router v6'
+  }
+};
+
+function Posts() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h2>Blog</h2>
+      {/* render any matching child */}
+      <Outlet />
+    </div>
+  );
+}
+
+function Post() {
+  const { slug } = useParams();
+  const post = BlogPosts[slug];
+  const { title, description } = post;
+  return (
+    <div style={{ padding: 20 }}>
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </div>
+  );
+}
+
+function PostLists() {
+  return (
+    <ul>
+      {Object.entries(BlogPosts).map(([slug, { title }]) => (
+        <li key={slug}>
+          <Link to={`/posts/${slug}`}>
+            <h3>{title}</h3>
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+}
 
 function Home() {
   return (
@@ -30,11 +77,18 @@ function App() {
     </Link>
     <Link to="/about" style={{ padding: 5 }}>
       About
+    </Link>    
+    <Link to="/posts" style={{ padding: 5 }}>
+      Posts
     </Link>
   </nav>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path='/about' element={<About />} />
+        <Route path="posts" element={<Posts />}>
+          <Route path="/" element={<PostLists />} />
+          <Route path=":slug" element={<Post />} />
+        </Route>
       </Routes>
     </Router>
   );
